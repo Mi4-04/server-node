8bed8d4dae75e9ba7785e372af6dca906b29a006
Zakharov Misha <co6a4ken01@mail.ru>
2020-11-10 19:43:28 +0300

    orders pages

--- User.js
+++ User.js
@@ -5,32 +5,44 @@ const sequelize = new Sequelize("servernode", "zakharovm", "zakharovm", {
     port: 5432
 })
 
-const Users = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    surname: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    }
-})
-
-sequelize.sync().then(() => console.log("Database is ready"))
+const Users = sequelize.define('Users', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    isAlpha: true,
+  },
+  surname: {
+    type: DataTypes.STRING,
+    isAlpha: true, 
+  },
+  country: {
+    type: DataTypes.STRING,
+    isAlpha: true, 
+  },
+  city: {
+    type: DataTypes.STRING,
+    isAlpha: true, 
+  },
+  typeUser: {
+    type: DataTypes.ENUM('iCustomer', 'iExecutor') ,
+  
+  },
+  
+});
 
 exports = module.exports = {
     sequelize,
